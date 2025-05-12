5b24080df3e59224a185d985147dbf8f59eb9ab2
TheObtey <95309102+TheObtey@users.noreply.github.com>
2025-05-12 07:24:43 +0000

    Remove test route

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ console.log('express.json is loaded');
 app.use(express.json());
 
 
-// Test
-app.get('/test-db', (req, res) => {
-    db.query('SELECT NOW()', (err, result) => {
-	if (err) return res.status(500).send(err);
-	res.send(result);
-    });
-});
-
-
 // Routes
 app.use('/', authRoutes); // Redirige vers http://localhost:3000/api
 app.use('/passwords', passwordRoutes);
