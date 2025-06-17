4758a0226ac5c66c11c56b1ee677da54591a64a3
root <root@LAPTOP-OBTEY.localdomain>
2025-06-17 15:34:11 +0200

    Add '/update-master-password' route.

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,45 @@ const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
 const authController = require('../controllers/authController');
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.put('/update-master-password', verifyToken, (req, res) => {
+    const userId = req.user.id;
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        return res.status(400).json({ error: 'Les champs oldPassword et newPassword sont obligatoires' });
+    }
+
+    try {
+        const checkQuery = `SELECT password_hash FROM users WHERE id = ?`;
+        
+        db.query(checkQuery, [userId], async (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: 'Erreur lors de la vérification de l\'utilisateur', details: err });
+            }
+
+            const isMatch = await bcrypt.compare(oldPassword, result[0].password_hash);
+            if (!isMatch) {
+                return res.status(401).json({ error: 'Mot de passe incorrect' });
+            }
+
+            const hash = await bcrypt.hash(newPassword, 10);
+            const updateQuery = `UPDATE users SET password_hash = ?, password = ? WHERE id = ?`;
+            db.query(updateQuery, [hash, newPassword, userId], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Erreur lors de la mise à jour du mot de passe', details: err });
+                }
+
+                res.status(200).json({ message: 'Mot de passe mis à jour' });
+            });
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Erreur lors de la mise à jour du mot de passe', details: err });
+    }
+});
 router.delete('/nuke', verifyToken, (req, res) => {
     const userId = req.user.id;
 
